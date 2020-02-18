318edc18467c7880f4fc0ef05b0edb0016162be5
Thiago Lucena <thiago@teraidc.com.br>
2020-02-18 16:27:55 -0300

    API Imagens

--- modulos/images.js
+++ modulos/images.js
@@ -1,37 +1,37 @@
 module.exports = app => {
     const fs = require('graceful-fs');
-    const dir_app = process.cwd();
+    const config = require('../config');
     var sqlite3 = require('sqlite3').verbose();
     app.get('/categoria/imagem', (req, res) => {
-        dir='Dados/imagens';
+        dir=config.dir_app+'/Dados/imagens';
         var files = fs.readdirSync(dir);
         cat=[];
         for (var i in files){
           var name = dir + '/' + files[i];
           if (fs.statSync(name).isDirectory()){
-            cat.push(name);
+            cat.push(name.replace(config.dir_app+'/',''));
           }
         }
         res.json({
             "status":"successo",
             "data":cat
         })
-    })
-}
-
-//Lista Imagens
-function lista_imagem(dir){
-  $('#preview-imagens').html('');
-  var files = fs.readdirSync(dir);
-  for (var i in files){
-    var name = dir + '/' + files[i];
-    if (fs.statSync(name).isDirectory()){
-      //É um diretorio
-    }else{
-      img=name;
-      img=img.replace('#','%23');
-      $('#preview-imagens').append('<li><img src="'+img+'" onclick="background(\''+btoa(img)+'\')"></li>')
+  })
+  app.get('/categoria/imagem/:dir', (req, res) => {
+    cat= req.params.dir;
+    dir=config.dir_app+'/Dados/imagens/'+cat;
+    var files = fs.readdirSync(dir);
+    cat=[];
+    for (var i in files){
+      var name = dir + '/' + files[i];
+      if (fs.statSync(name).isDirectory()){
+      }else{
+        cat.push(name.replace(config.dir_app+'/',''));
+      }
     }
-  }
-  $('#current_loading').html('Carregado Preview de Imagens');
+    res.json({
+        "status":"successo",
+        "data":cat
+    })
+  })
 }
